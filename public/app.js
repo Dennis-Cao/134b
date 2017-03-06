25713c114a9a1ffb21d06d4095f2e9a7255ccd69
Dennis S Cao <dscao@acs-cseb250-11.ucsd.edu>
2017-03-05 16:53:08 -0800

    logoutbuttn attempt

--- public/app.js
+++ public/app.js
@@ -19,6 +19,7 @@ console.log(defaultApp.name);  // "[DEFAULT]"
 const txtEmail = document.getElementById('txtemail');
 const txtPassword = document.getElementById('txtpassword');
 const btnLogin = document.getElementById('btnLogin');
+const btnLogout = document.getElementById('btnLogout');
 // You can retrieve services via the defaultApp variable...
 //var defaultStorage = defaultApp.storage();
 var defaultDatabase = defaultApp.database();
@@ -34,7 +35,9 @@ btnLogin.addEventListener('click',e=>{
 	promise.catch(e=> console.log(e.message));
 })
 
-
+btnLogout.addEventListener('click', e=>{
+	firebase.auth().signOut();
+})
 
 
 
