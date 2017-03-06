0e5153cf438fb99bb1eaf508c765e323a138742a
Dennis S Cao <dscao@acs-cseb250-11.ucsd.edu>
2017-03-05 18:32:45 -0800

    bla'

---
+++ public/logout.js
@@ -0,0 +1,81 @@
+
+var config = {
+	    apiKey: "AIzaSyBoUFCtMwn6r63MEd8pfQLTMLp_h-KLkWY",
+	    authDomain: "cse-134b-103ee.firebaseapp.com",
+	    databaseURL: "https://cse-134b-103ee.firebaseio.com",
+	    storageBucket: "cse-134b-103ee.appspot.com",
+	    messagingSenderId: "737274665905"
+	  };
+	  //firebase.initializeApp(config);
+
+	  // Initialize the default app
+var defaultApp = firebase.initializeApp(config);
+
+
+
+
+// console.log(typeof defaultApp); 
+// console.log(defaultApp.name);  // "[DEFAULT]"
+// var txtEmail = document.getElementById('txtemail');
+// var txtPassword = document.getElementById('txtpassword');
+// const btnLogin = document.getElementById('btnLogin');
+// const btnLogout = document.getElementById('btnLogout');
+// You can retrieve services via the defaultApp variable...
+//var defaultStorage = defaultApp.storage();
+// var defaultDatabase = defaultApp.database();
+
+
+//login event
+// btnLogin.addEventListener('click',e=>{
+// 	const email = txtEmail.value;
+// 	const pass = txtPassword.value;
+// 	const auth = firebase.auth();
+// 	//sign in
+// 	const promise = auth.signInWithEmailAndPassword(email,pass);
+// 	promise.catch(e=> console.log(e.message));
+// })
+
+// btnLogout.addEventListener('click', e=>{
+// 	firebase.auth().signOut();
+// })
+
+var signout = function() {
+	firebase.auth().signOut();
+	window.location = './Login_pageBS.html';
+}
+
+
+
+var signin = function() {
+	var email = document.getElementById('txtemail');
+	var password = document.getElementById('txtpassword');
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    	alert("Incorrect combination")
+  });
+}
+var googleSignin = function(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    var user = result.user;
+  }).catch(function(error) {
+    alert("Google login did not work")
+  });
+}
+// var provider = new firebase.auth.GoogleAuthProvider();
+// function googleSignin() {
+//    firebase.auth()
+   
+//    .signInWithPopup(provider).then(function(result) {
+//       var token = result.credential.accessToken;
+//       var user = result.user;
+		
+//       console.log(token)
+//       console.log(user)
+//    }).catch(function(error) {
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+		
+//       console.log(error.code)
+//       console.log(error.message)
+//    });
+// }
